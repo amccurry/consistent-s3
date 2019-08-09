f47d71580b3fdbe66627b82a9cecc40acb644b0d
Aaron McCurry <amccurry@gmail.com>
2019-08-09 17:32:48 -0400

    Fixed build script.

--- build-release.sh
+++ build-release.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+set -ex
 VERSION="${1}"
-cd ./release/
+DIR=$(pwd)
+TMPDIR="/tmp/$(uuidgen)"
+mkdir $TMPDIR
+cd $TMPDIR
+
+git clone git@github.com:amccurry/consistent-s3.git
+cd $TMPDIR/consistent-s3
 git checkout repository
 git pull
 mvn install:install-file \
  -DgroupId=consistent-s3 \
  -DartifactId=consistent-s3 \
  -Dversion=${VERSION} \
- -Dfile=../target/consistent-s3-$VERSION.jar \
+ -Dfile=${DIR}/target/consistent-s3-$VERSION.jar \
  -Dpackaging=jar \
  -DgeneratePom=true \
  -DlocalRepositoryPath=. \
  -DcreateChecksum=true
+git add consistent-s3
+git commit -a -m "Repo release."
+git push
